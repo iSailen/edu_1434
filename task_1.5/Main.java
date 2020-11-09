52161ddefc7c68040134ea7c3f6608cb9fcab22c
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-09 11:49:10 +0300

    Create Main.java
    5 задание

---
+++ task_1.5/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+         Scanner scan = new Scanner(System.in);
+        System.out.println("Input temperature C4");
+                int cels_deg = scan.nextInt();
+        System.out.println("Tempature in F");
+        System.out.println(cels_deg+32);
+         }
+    }
