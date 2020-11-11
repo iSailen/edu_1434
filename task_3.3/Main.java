19f3eccf924e5335a75871f284d03dbe2c1f4e21
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-11 13:12:22 +0300

    Create Main.java

---
+++ task_3.3/Main.java
@@ -0,0 +1,15 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа.
+Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(intCompare(1, 2));
+        
+    }
+    public static boolean intCompare(int a, int b) {
+        return a==b;
+    }
+
+}
