a221f8de103893952fb5b0821082f6ec1b8c3507
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-19 19:09:17 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -1,3 +1,4 @@
+
 /*Выведи на экран таблицу умножения 10 на 10 в следующем виде:
 1 2 3 4 ...
 2 4 6 8 ...
@@ -9,12 +10,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] pif = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int q : pif) {
-            for (int w : pif) {
-                System.out.printf("%4d", (q * w));
+        for (int i = 1;i<10;i++) {
+                for (int j = 1;j<10; j++ ) { 
+                    System.out.printf("%4d", (i * j));
             }
-            System.out.println();
+           System.out.println();
         }
     }
 }
