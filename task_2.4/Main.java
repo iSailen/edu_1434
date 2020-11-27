44c28bcf8cf87e51852266eb56c4c3a7d7784385
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-27 22:06:06 +0300

    Update Main.java
    переделано

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -1,19 +1,21 @@
 /*
+2.4
 Задание: Пользователь вводит сумму вклада и процент, который будет начисляться ежегодно. Отобразить размер вклада поочередно на ближайшие 5 лет.
 */
-
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите сумму: ");
-        double sum = scanner.nextInt();
-        System.out.println("Размер вашего вклада поочередно на ближайшие 5 лет: ");
-        for (int i = 1; i <= 5; i++) {
-            sum = sum * 1.05;
-            System.out.println(sum);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите сумму вклада: ");
+        double deposit = scan.nextDouble();
+        System.out.println("Введите % доходности годовой: ");
+        double interest = scan.nextDouble();
+        for (int i = 1;i<=5;i++) {
+            deposit=deposit+(deposit*(interest/100));
+
+            System.out.println("Сумма вклада после "+i+" года составляет: "+String.format("%.2f",deposit));
         }
+
+
     }
 }
