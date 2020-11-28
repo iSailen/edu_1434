3bafd5f5e38a754ae1d7512c8b822a74ab25ca50
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-28 20:57:04 +0300

    Update Main.java
    переделано

--- task_3.3/Main.java
+++ task_3.3/Main.java
@@ -1,15 +1,18 @@
 /*
-Задание: Сделайте функцию, которая параметрами принимает 2 числа.
-Если эти числа равны - пусть функция вернет true, а если не равны - false.
+3.3
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
 */
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(intCompare(1, 2));
-        
-    }
-    public static boolean intCompare(int a, int b) {
-        return a==b;
+    public static boolean compare(int a, int b) {
+        if (a == b) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    public static void main(String[] args) {
+        System.out.println(compare(1, 2));
+        System.out.println(compare(1, 1));
+    }
 }
