d49634447bb4b5fb29145ca1ef26b3ca7176b3e6
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-28 17:55:37 +0300

    Update Main.java
    переделано

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -1,16 +1,14 @@
 /*
+2.9
 Задание: Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.
 */
-
 public class Main {
     public static void main(String[] args) {
         int numbers[] = {1,2,3,4,5};
-        boolean flag = false;
-        int itogo =1;
+        String result="нет";
         for (int i = 0; i < numbers.length; i++) {
-if (numbers[i]==5) flag=true;
+            if (numbers[i]==5) {result="да";}
         }
-        if (flag==true) System.out.println("Да");
-        else System.out.printf("Нет");
-        }
+        System.out.println(result);
     }
+}
