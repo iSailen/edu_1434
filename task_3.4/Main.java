69cb1dad82a4084d9413404a238738cc39214f4c
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-28 21:04:52 +0300

    Update Main.java
    переделано

--- task_3.4/Main.java
+++ task_3.4/Main.java
@@ -1,15 +1,14 @@
 /*
-Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 -
-пусть функция вернет true, а если нет - false.
+3.4
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
 */
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(sumCompare(3, 7));
-
+    public static boolean fun34(int a, int b) {
+        if (a+b>10){return true;}
+        else {return false;}
     }
-
-    public static boolean sumCompare(int a, int b) {
-        return (a + b) > 10;
+    public static void main(String[] args) {
+        System.out.println(fun34(1,8));
+        System.out.println(fun34(5,8));
     }
 }
