925c48db68a9c0873b51f2a9c01a61eb20e2d01a
iSailen <73995191+iSailen@users.noreply.github.com>
2020-11-28 18:06:44 +0300

    Update Main.java
    переделано

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -1,22 +1,18 @@
 /*
+2.11
 Задание: Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
 */
 
 public class Main {
     public static void main(String[] args) {
-        int numbers[] = {1,4,4,4,5};
-        int cnt = 0;
-        int total = 0;
-       // boolean flag = false;
+        int numbers[] = {1,2,3,4,5,6,7,8,9};
+        int num=0;
+        int cnt=0;
         for (int i = 0; i < numbers.length; i++) {
-if (total<=10)
-    {
-        total=numbers[i]+total;
-        cnt++;
-    }
+            num=num+numbers[i];
+            if (num>10){ cnt=i+1;break;}
         }
         System.out.println(cnt);
-        // if (flag==true) System.out.println("Да");
-       // else System.out.printf("Нет");
-        }
     }
+
+}
